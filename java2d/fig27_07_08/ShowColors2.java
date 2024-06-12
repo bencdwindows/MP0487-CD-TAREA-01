86a3da832fb463178a55747c718f61ec3b63031f
Rubén Aragunde <robben_fly_22@hotmail.com>
2024-06-12 16:55:49 +0200

    "Commit del Clone Fork"

--- java2d/fig27_07_08/ShowColors2.java
+++ java2d/fig27_07_08/ShowColors2.java
@@ -5,6 +5,8 @@ import javax.swing.JFrame;
 public class ShowColors2 {
    // execute application
    public static void main(String[] args)  {
+      //Haremos un println para generar algún cambio en el clon del fork
+      System.out.println("Segundo cambio del fork");
       ShowColors2JFrame application = new ShowColors2JFrame();
       application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
    } 
